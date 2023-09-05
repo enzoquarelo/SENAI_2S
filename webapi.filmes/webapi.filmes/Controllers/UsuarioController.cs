c608d6d3a4d252dbd3e31cfc8855ef08df6f16a3
enzoquarelo <enzo.quarelo@gmail.com>
2023-09-05 08:03:20 -0300

    JWT aplication

---
+++ webapi.filmes/webapi.filmes/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.filmes.DataTransfering;
+using webapi.filmes.Domains;
+using webapi.filmes.Interfaces;
+using webapi.filmes.Repositories;
+
+namespace webapi.filmes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UsuarioTransfer dadosUsuario)
+        {
+            try
+            {
+                UsuarioDomain usuarioEncontrado = _usuarioRepository.Login(dadosUsuario.Email, dadosUsuario.Senha);
+
+                if (usuarioEncontrado == null)
+                {
+                    return NotFound("Usuário nao encontrado!");
+                }
+
+                return Ok(usuarioEncontrado);
+            }
+            catch (Exception err)
+            {
+                return BadRequest(err);
+            }
+        }
+    }
+}
--- webapi.filmes/webapi.filmes/Repositories/UsuarioRepository.cs
+++ webapi.filmes/webapi.filmes/Repositories/UsuarioRepository.cs
@@ -7,10 +7,47 @@ namespace webapi.filmes.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
-        private string stringConexao = "Data Source = DESKTOP-2KJISQH\\SENAI; Initial Catalog = Filmes; User Id = sa; Pwd = Senai@134";
+        private string StringConexao = "Data Source = DESKTOP-2KJISQH\\SENAI; Initial Catalog = Filmes; User Id = sa; Pwd = Senai@134";
 
-        // autenticação através do windows: Integrated Security = true
+        /// <summary>
+        /// É o método que permite a autenticação do usuário
+        /// </summary>
+        /// <param name="email">O e-mail do usuário</param>
+        /// <param name="senha">A senha do usuário</param>
+        /// <returns>Os dados do usuário em um objeto</returns>
+        public UsuarioDomain Login(string email, string senha)
+        {
+            using (SqlConnection connection = new SqlConnection(StringConexao))
+            {
+                string query = "SELECT Id, Nome, Email, Senha, IsAdmin FROM Usuario " +
+                               "WHERE Email = @Email AND Senha = @Senha";
 
+                connection.Open();
 
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Senha", senha);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        UsuarioDomain usuarioEncontrado = new UsuarioDomain()
+                        {
+                            IdUsuario = Convert.ToInt32(reader["Id"]),
+                            Nome = reader["Nome"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Senha = reader["Senha"].ToString(),
+                            IsAdmin = Convert.ToBoolean(reader["IsAdmin"])
+                        };
+
+                        return usuarioEncontrado;
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
