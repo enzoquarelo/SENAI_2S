c82328904e010fc2a791b60a80cb7d683c74a0b2
enzoquarelo <enzo.quarelo@gmail.com>
2023-09-19 10:16:37 -0300

    domains finalizadas

---
+++ 2Sprint_API/Event+ API/webapi.event+.senai/Contexts/EventContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.event_.senai.Domains;
+
+namespace webapi.event_.senai.Contexts
+{
+    public class EventContext : DbContext
+    {
+        public DbSet<TiposUsuario> TiposUsuario { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<TiposEvento> TiposEvento { get; set; }
+
+        public DbSet<Evento> Evento { get; set; }
+
+        public DbSet<ComentariosEvento> ComentariosEvento { get; set; }
+
+        public DbSet<Instituicao> Instituicao { get; set; }
+
+        public DbSet<PresencasEvento> PresencasEvento { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2KJISQH\\SENAI; Database= webapi.event+.manha; User Id= sa; Pwd= Senai@134; TrustServerCertificate= True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
