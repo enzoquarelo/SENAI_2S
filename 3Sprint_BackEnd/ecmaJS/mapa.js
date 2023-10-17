d73b2c5eb03ac20848379c046fda1f4a1f3d7dc9
enzoquarelo <enzo.quarelo@gmail.com>
2023-10-17 10:39:16 -0300

    js estudos

---
+++ 3Sprint_BackEnd/ecmaJS/mapa.js
@@ -0,0 +1,11 @@
+const mesa = ['Julia', 'Enzo', 'Paulo', 'Lucas', 'Carlos', 'Eduardo'];
+
+mesa.forEach( (cadaPessoa) => {
+        console.log('Bom Dia ' + cadaPessoa);
+});
+
+// const dobro = (x) => {
+//     return x * 2;
+// }
+
+// console.log( dobro(9))
