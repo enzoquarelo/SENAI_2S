da1bffedeecc96ef4f238d570e41a256158465d7
enzoquarelo <enzo.quarelo@gmail.com>
2023-10-27 10:56:38 -0300

    react destructing

---
+++ sprint_3-FrontEnd/estudos_react/hello/src/App.jsx
@@ -0,0 +1,26 @@
+import Title from './components/Title/Title'
+import CardEvento from './components/CardEvento/CardEvento'
+import Container from './components/Container/Container'
+
+import './App.css';
+
+function App() {
+  return (
+    //Criar as propriedades titulo, texto, textolink
+    //passar as propriedades em cada um dos 3 componentes
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Title nome="Enzo" sobrenome="Quarelo" />
+
+      <br /><br />
+
+      <Container>
+        <CardEvento titulo="C# do Carlão" texto="Listinha de 10 exercício" link="Conecte-se" />
+        <CardEvento titulo="Js com Dudu" texto="Bora codar em JS" link="Conecte-se" />
+        <CardEvento titulo="SQL com Carlão" texto="Vamo modelar?" link="Conecte-se" />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
